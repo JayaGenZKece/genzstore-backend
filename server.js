a335e6cc353527ca20ec0a2a61183297166aaf1f
GenZstore <jayasubkhan3@gmail.com>
2026-04-01 11:08:10 +0700

    update margin lagi

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const PAKASIR_SLUG = "genzstore";
 // KONFIGURASI MARGIN HARGA JUAL
 // 15% di atas harga modal GOLD TokoVoucher
 // ==========================================
-const MARGIN_PERSEN = 10;
+const MARGIN_PERSEN = 8;
 
 // ==========================================
 // KONFIGURASI SUPABASE
